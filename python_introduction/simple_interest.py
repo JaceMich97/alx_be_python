36bd3475836418e962a56ac053018ae4077f1c89
JaceMich97 <udokauzor67@gmail.com>
2025-07-12 00:54:09 +0100

    Add simple_interest.py: sim int c calculation task

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+# Definition of variables
+principal = 1000
+rate = 0.05
+time = 3
+
+# Formula to calculate simple interest earned on an investment over a period of time = (P * R * T) )
+interest = principal * rate * time
+print(f"The simple interest is: {interest}")
