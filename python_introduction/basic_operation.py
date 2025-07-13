e125a2481f2e2cf5c02a60da1902221c4f4d50b7
JaceMich97 <udokauzor67@gmail.com>
2025-07-13 16:08:33 +0100

    update calculations in basic_operation.py

--- python_introduction/basic_operation.py
+++ python_introduction/basic_operation.py
@@ -1,15 +1,18 @@
-
+# ASsign specific values to the variables
 number1 = 10
 number2 = 5
 
+# Perform addition
+#to perform di
 sum_result = number1 + number2
 
-
+# Perform subtraction
 difference_result = number1 - number2
 
-
+# Perform multiplication
 product_result = number1 * number2
 
+# Print the results in a human-readable format
 print(f"Addition of {number1} and {number2} is {sum_result}")
 print(f"Subtraction of {number1} and {number2} is {difference_result}")
 print(f"Multiplication of {number1} and {number2} is {product_result}")
