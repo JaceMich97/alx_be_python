2cc868796daff66ca0a0b27e67a285c8052bf31d
JaceMich97 <udokauzor67@gmail.com>
2025-07-30 20:44:31 +0100

    add test_simple_calculator.py: changes 2

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,10 +1,33 @@
-import unittest
+[20:37, 30/07/2025] Jace🙈: import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
         """Set up the calculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertEqual(self.calc.add(-2, -2), -4)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(-5, 2), -7)
+
+    def test_multiply(self):
+        self.assertE…
+[20:43, 30/07/2025] Jace🙈: import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Create a new calculator instance before each test."""
         self.calc = SimpleCalculator()
 
     def test_addition(self):
